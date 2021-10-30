cea26fb8837d81253db47f4d6386dbd91dd7b37d
Aman Porwal <amanporwal510@gmail.com>
2021-10-31 00:16:29 +0530

    add deploying link of rtmp and api server

--- src/component/streams/StreamShow.js
+++ src/component/streams/StreamShow.js
@@ -34,7 +34,7 @@ class StreamShow extends Component {
 
       this.player = flv.createPlayer({
          type: 'flv',
-         url: `http://localhost:8000/live/${id}.flv`
+         url: `https://streamy-rtmpserver.herokuapp.com/live/${id}.flv`
       });
       this.player.attachMediaElement(this.videoRef.current);
       this.player.load();
