7350b515260f9202b154f7b384e81f4ac05dfd01
Aman Porwal <amanporwal510@gmail.com>
2021-10-31 01:14:03 +0530

    fix: google auth button not visible

--- src/component/GoogleAuth.js
+++ src/component/GoogleAuth.js
@@ -36,9 +36,7 @@ class GoogleAuth extends Component {
    }
 
    renderAuthButton() {
-      if(this.props.isSignedIn == null) {
-         return null;
-      } else if( this.props.isSignedIn) {
+      if(this.props.isSignedIn) {
          return (
             <button onClick={this.onSignOutClick} className='ui red google button' >
                <i className='google icon' />
